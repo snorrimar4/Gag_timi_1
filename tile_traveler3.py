ddedd78c0ced250c5abd75139b5e2cb8415c1df5
Snorri Már Gunnarsson <70167747+snorrimar4@users.noreply.github.com>
2023-01-10 10:24:07 +0000

    Add files via upload

---
+++ tile_traveler3.py
@@ -0,0 +1,42 @@
+'''
+TODO: Player Class, getur valið nafn
+TODO: Board?
+TODO: Tile Class
+TODO: Lever Class
+TODO: Random coins, FASTI SEM HELDUR UTAN UM FJÖLDA PENINGA
+TODO: Grid implementation
+TODO: Random coin amont
+TODO: Stækkanlegt map
+TODO: Miðjureitir eru upp,niður,hægri, vinstri
+TODO: Vinstri veggir: leyfa ekki að fara til vinstri, sama með hægri veggi og top og bottom veggi. Leyfa ekki að fara í þá átt sem þeir snúa í.
+TODO: Player er með update og get current position
+TODO: Winning position is always (max,min)
+'''
+import random
+STARTING_POINT = (1,1)
+AMOUNT_OF_COINS = random.randint(1,100)
+
+class Tile:
+    def __init__(self) -> None:
+        pass
+
+class Player:
+    def __init__(self, placement, coins = 0) -> None:
+        self.name = input('Enter a player name: ')
+        self.placement = placement
+        self.coins = coins
+
+    def __str__(self) -> str:
+        return f"Player: {self.name}, Coins: {self.coins}\nCurrent Position: {self.placement}"
+
+class Lever:
+    def __init__(self, coin) -> None:
+        pass
+
+class Grid:
+    def __init__(self) -> None:
+        pass
+
+p1 = Player(STARTING_POINT)
+#p2 = Player(STARTING_POINT)
+print(p1)
