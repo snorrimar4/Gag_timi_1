55ade03c504d76923e71b72fb96c75c4f364d439
LovisaBa <lovisabaldvins@gmail.com>
2023-01-10 10:59:28 +0000

    player class

--- player_class.py
+++ player_class.py
@@ -4,5 +4,36 @@ class Player:
         self.placement = placement
         self.coins = 0
 
+
     def __str__(self) -> str:
         return f"Player: {self.name}, Coins: {self.coins}\nCurrent Position: {self.placement}"
+
+
+    def get_move(self, valid_move:list)->str:
+        '''Gets the from the player'''
+        print("You can move ", end ="")
+        for direction in valid_move:
+            print(direction,end=", ")
+        movement = input("Enter a direction")
+        while not self.check_move(movement, valid_move):
+            print("Please pick a valid direction.")
+            movement = input("Enter a direction")
+    
+
+    def check_move(self,movement, valid_moves):
+        '''Checks if a movement is valid'''
+        if movement in valid_moves:
+            return True
+        return False
+
+
+    def move(self, movement):
+        '''updates the current placement depending on the direction moved'''
+        if movement == "n":
+            pass
+        elif movement == "s":
+            pass
+        elif movement == "e":
+            pass
+        elif movement == "w":
+            pass
