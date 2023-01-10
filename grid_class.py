1bfe01321c51caed5a1dcafa73a4f64aeea0b3e2
Alessandro Manfrini <alessandro.manfrini1@gmail.com>
2023-01-10 10:54:05 +0000

    grid

--- grid_class.py
+++ grid_class.py
@@ -1,6 +1,12 @@
 class Grid:
-    def __init__(self, dimension) -> None:
-        self.dimension = tuple(input("Enter the grid's dimension: "))
+    def __init__(self) -> None:
+        self.x = int(input("Enter the grid's X value: "))
+        self.y = int(input("Enter the grid's Y value: "))
+        self.dimension = self.x, self.y
 
     def __str__(self):
-        return f"{self.dimension}"
+        return f"Grid's dimension: {self.x}x{self.y}"
+
+
+g1 = Grid()
+print(g1)
